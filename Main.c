4303c3bbf738d73df9f8d69fd65ce4400f994f93
NervesOfJello <cole.bonnema@loop.colum.edu>
2019-09-16 13:00:37 -0500

    Created Program

---
+++ Main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h> //included for exit() calls 
+
+int main()
+{
+	int bottomFloor = 1;
+	int topFloor = 9;
+	int currentFloor = 1;
+	int targetFloor = 1;
+    
+    //declare floor boundaries
+	printf("This elevator can go from floor %d", bottomFloor);
+	printf(" to floor %d\n", topFloor);
+	
+	//get current floor from user
+	printf("What floor are you on? ");
+	if (scanf("%d", &currentFloor) > 0) //ensure input is valid
+	{
+		if (currentFloor <= topFloor && currentFloor >= bottomFloor) //check that current floor is within boundaries
+		{
+			printf("You are on floor %d\n", currentFloor);
+		}
+		else
+		{
+			printf("I'm sorry, that floor doesn't exist.");
+			exit(0);
+		}
+	}
+	else
+	{
+		printf("ERROR: You did not enter a number.");
+		exit(0);
+	}
+
+    //get target floor from user
+	printf("What floor would you like to go to? ");
+	if (scanf("%d", &targetFloor) > 0)
+	{
+		if (targetFloor == currentFloor) //make sure it's not just the same floor
+		{
+			printf("\nYou're already there!");
+		}
+		else if (targetFloor <= topFloor && targetFloor >= bottomFloor) //check boundaries
+		{
+			printf("\n");
+			if (targetFloor < currentFloor) //going down
+			{
+				for (currentFloor; currentFloor > targetFloor; currentFloor--)
+				{
+					printf("%d...\n", currentFloor);
+				}
+			}
+			else //going up
+			{
+				for (currentFloor; currentFloor < targetFloor; currentFloor++)
+				{
+					printf("%d...\n", currentFloor);
+				}
+			}
+			printf("You have arrived on floor %d", currentFloor);
+			
+		}
+		else //OOB target floor
+		{
+			printf("I'm sorry, that floor doesn't exist.");
+			exit(0);
+		}
+	}
+	else //invalid input
+	{
+		printf("ERROR: You did not enter a number.");
+		exit(0);
+	}
+
+	return 0;
+}
